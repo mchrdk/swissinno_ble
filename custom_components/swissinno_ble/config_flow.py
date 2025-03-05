ad391f5281f4dda144bbad6fdcff7a37c3a7d7ac
unigas <markus@samuelson.nu>
2025-03-05 21:01:57 +0100

    Add files via upload
    Updated config_flow.py

--- custom_components/swissinno_ble/config_flow.py
+++ custom_components/swissinno_ble/config_flow.py
@@ -1,14 +1,16 @@
 import logging
 import voluptuous as vol
+
 from homeassistant import config_entries
 from homeassistant.core import callback
-
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 class SwissinnoBLEConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for SWISSINNO BLE."""
+    """Handle a config flow for Swissinno BLE."""
+
+    VERSION = 1
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
@@ -20,22 +22,28 @@ class SwissinnoBLEConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({}),
+            description_placeholders={"integration_name": "SWISSINNO BLE"},
             errors=errors,
         )
 
     @staticmethod
     @callback
-    def async_get_options_flow(entry):
-        """Return the options flow."""
-        return SwissinnoBLEOptionsFlow(entry)
+    def async_get_options_flow(config_entry):
+        """Return the options flow handler."""
+        return SwissinnoBLEOptionsFlowHandler(config_entry)
 
-class SwissinnoBLEOptionsFlow(config_entries.OptionsFlow):
-    """Handle options flow for SWISSINNO BLE."""
 
-    def __init__(self, entry):
+class SwissinnoBLEOptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle options for Swissinno BLE."""
+
+    def __init__(self, config_entry):
         """Initialize options flow."""
-        self.entry = entry
+        self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
-        """Manage the options for the integration."""
-        return self.async_create_entry(title="", data={})
+        """Manage the options."""
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({}),
+            description_placeholders={"integration_name": "SWISSINNO BLE"},
+        )
